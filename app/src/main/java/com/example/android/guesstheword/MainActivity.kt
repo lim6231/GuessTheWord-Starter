c9472f41538ae20d819971f9d010c18cd3793bd5
lim6231 <lim6231@hotmail.com>
2019-12-06 14:39:54 +0800

    init

--- app/src/main/java/com/example/android/guesstheword/MainActivity.kt
+++ app/src/main/java/com/example/android/guesstheword/MainActivity.kt
@@ -22,7 +22,7 @@ import androidx.appcompat.app.AppCompatActivity
 /**
  * Creates an Activity that hosts all of the fragments in the app
  */
-//
+///
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
